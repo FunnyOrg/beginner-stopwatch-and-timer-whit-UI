6a37acb51285a0f12f3fe31207b624913a6fb13c
Doh <83902568+domxst@users.noreply.github.com>
2021-05-15 18:03:27 +0200

    Update TimerGUI.java

--- asset/TimerGUI.java
+++ asset/TimerGUI.java
@@ -1,97 +1,134 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+package asset;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TimerGUI extends JPanel {
-    JLabel promptLabel, timerLabel;
-    final int count = 30;
-    JButton start;
-    JButton end;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+
+public class TimerGUI {
+	
+	boolean started = false;
+    JLabel label, timerLabel;
+    JButton start = new JButton("start");
+    JFrame window = new JFrame("Timer");
+    JButton end = new JButton("end");
+    int counter = 30;
     Timer timer;
-
+    Font font = new Font("Verdana", Font.PLAIN, 15);
+    
     public TimerGUI() {
+    	
+    	timer = new Timer(1000, new ActionListener() {
+    		
+    		public void actionPerformed(ActionEvent e) {
+    			counter--;
+    			if(getCounter() == 0) {
+    				fermo();
+    			}
+    			label.setText("   Tempo rimasto: " + getCounter());
+    		}
+    		
+    	});
+    	
+    	label = new JLabel("   Tempo rimasto: 0");
+    	label.setBounds(50, 60, 180, 30);
+        label.setFont(new Font("Verdana", Font.PLAIN, 15));
+        label.setEnabled(true);
+        label.setVisible(true);
+        label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        label.setOpaque(true);
         
-        JFrame myframe = new JFrame("Timer");
+        start.setBounds(35, 150, 100, 30);
+        start.setFont(font);
+        start.setFocusable(false);
+       
+        //start.setEnabled(true);
         
-        myframe.getContentPane().setBackground(Color.GRAY);
-        myframe.setLayout(null);  
-        myframe.pack();
-        myframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        myframe.setSize(300,300);
-        myframe.setResizable(false);
-        myframe.setVisible(true);
-
-
-
-        setLayout(new GridLayout(0,2));
-
-        start = new JButton("Start");
-        add(start);
-        Event e = new Event();
-        start.addActionListener(e);
-
-        end = new JButton("End");
-        end.setEnabled(false);
-        add(end);
-        end.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                timer.stop();
-                start.setEnabled(true);
-                end.setEnabled(false);
-                timerLabel.setText("Time Remaining " + count + " seconds");
-
-            }
-
+        
+        start.addActionListener(new ActionListener() {
+        	
+        	public void actionPerformed(ActionEvent e) {
+        		
+        		//if(e.getSource() == start) {
+        			if(started == false) {
+        				started = true;
+        				start.setText("Stop");
+        				avvia();
+        			} else {
+        				started = false;
+        				start.setText("Start");
+        				fermo();
+        			}
+        	//	}
+        	
+        		//start.setEnabled(false);
+        		//end.setEnabled(true);
+        		
+        	}
+        	
         });
+        
+        end.setBounds(150, 150, 100, 30);
+        end.setFont(font);
+        end.setFocusable(false);
+        
+        //end.setEnabled(false);
+        
+        end.addActionListener(new ActionListener() {
+        	
+        	public void actionPerformed(ActionEvent e) {
+        		reset();
+        		
+        		//end.setEnabled(false);
+        		//start.setEnabled(true);
+        		
+        	}
+        	
+        });
+        
+        
+        window.add(start);
+        window.add(end);
+        window.add(label);
 
-        timerLabel = new JLabel("Time Remaining 30 seconds");
-        add(timerLabel);
-
+        window.getContentPane().setBackground(Color.GRAY);
+        window.setLayout(null);  
+        window.pack();
+        window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        window.setSize(300,300);
+        window.setResizable(false);
+        window.setVisible(true);
+    	
     }
-
-    private class Event implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            start.setEnabled(false);
-            end.setEnabled(true);
-            timerLabel.setText("Time Remaining " + count + " seconds");
-
-            TimeClass tc = new TimeClass(count);
-            timer = new Timer(1000, tc);
-            timer.start();
-
-        }
+    
+    public void avvia() {
+    	timer.start();
     }
-
-    private class TimeClass implements ActionListener {
-        int counter;
-
-        public TimeClass(int count) {
-            this.counter = count;
-
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            counter--;
-            
-            
-            if (counter >= 1) {
-                timerLabel.setText("Time Remaining " + counter + " seconds");
-            } else {    //Aggiuntere TrayIconDemo per l'avviso
-                timer.stop();
-                timerLabel.setText("game over");
-            }
-
-        }
+    
+    public void fermo() {
+    	timer.stop();
+    	label.setText("   Tempo rimasto: " + getCounter());
     }
-
-  
-
-
-
+    
+    public void reset() {
+    	setCounter(0);
+    	label.setText("   Tempo rimasto: " + getCounter());
+    }
+    
+    public void setCounter(int counter) {
+    	this.counter = counter;
+    }
+    
+    public int getCounter() {
+    	return counter;
+    }
+    
 }
+
