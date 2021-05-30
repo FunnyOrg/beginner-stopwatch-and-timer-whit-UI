f7a377ab4d24dae37298d212b109ed9e860f01f4
AntonioDev <76062159+ForTony2@users.noreply.github.com>
2021-05-30 11:45:38 +0200

    Update Main.java

--- asset/Main.java
+++ asset/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     
     public static void main(String args[]) {
-        Menu menu = new Menu();
-        //Stopwatch cronometro = new Stopwatch();
+        
+        new Menu();
+       
         
     }
 }
